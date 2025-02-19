5f99f9dee7253def9709d10f3a1b675262bbd7c7
caroline <carolineribeiro1996@gmail.com>
2025-02-19 09:02:35 -0300

    Estilizar Card Temas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+
 import { AuthContext, AuthProvider } from './contexts/AuthContext'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
@@ -19,6 +21,7 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
